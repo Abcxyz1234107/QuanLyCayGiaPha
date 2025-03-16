ddb28c055c13f6da911b0062fec2f3613dba344b
Abcxyz1234107 <minecraftzcc6@gmail.com>
2025-03-16 14:18:33 +0700

    Add project files.

---
+++ Model/DataProvider.cs
@@ -0,0 +1,25 @@
+﻿using QuanLyCayGiaPha.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachSan.Model
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get { if (_ins == null) _ins = new DataProvider(); return _ins; }
+            set { _ins = value; }
+        }
+
+        public CayGiaPhaEntities GP {  get; set; }
+        private DataProvider()
+        {
+            GP = new CayGiaPhaEntities();
+        }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyCayGiaPha.ViewModel
+{
+    public class MainWindowViewModel: BaseViewModel
+    {
+        public MainWindowViewModel() 
+        {
+            MessageBox.Show("Test main window view model => Da chay Thanh cong");
+        }
+    }
+}
